a23f94b986d3f32d62706874d120a35e3cd868df
Mario-Coxe <cox3site59@gmail.com>
2023-11-12 21:03:30 +0100

    add relationship in migartion comments with momments

--- database/migrations/1699819123319_comments.ts
+++ database/migrations/1699819123319_comments.ts
@@ -1,11 +1,14 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class extends BaseSchema {
+export default class Comments extends BaseSchema {
   protected tableName = 'comments'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('username')
+      table.string('text')
+      table.integer('moment_id').unsigned().references('moments.id').onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
@@ -15,7 +18,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
